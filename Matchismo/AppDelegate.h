fd10b5842382765c4c7318accdfe050e9ddc1f70
Lowell Bander <lowellbander@gmail.com>
2015-03-26 09:27:00 -0700

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Lowell Bander on 3/26/15.
+//  Copyright (c) 2015 Lowell Bander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
